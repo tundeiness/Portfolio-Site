6e63d5780094ad29ed187693419d6517e982ffe6
tundeiness <tundeiness@yahoo.co.uk>
2020-06-25 16:10:44 +0100

    setup Card

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,8 +3,11 @@ import React from 'react';
 // import Hero from './Hero';
 // import Carousel from './Carousel';
 
-const Card = ({ props }) => (
-  <p>Card Item</p>
+const Card = ({ item }) => (
+  <div>
+    <img className="" src={item.imgSrc} alt={item.imgSrc} />
+    { item.selected && <Cardinfo title={item.title} Subtitle={item.Subtitle} link={item.link} />}
+  </div>
 );
 
 export default Card;
