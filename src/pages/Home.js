301248b87613e4da314c63da16278ccfb960b6ca
tundeiness <tundeiness@yahoo.co.uk>
2020-06-25 12:56:50 +0100

    setup hero contents

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import Hero from '../Components/Hero';
 
-const Home = ({ props }) => (
-  <p>You are Home!!</p>
+const Home = ({ title, subtitle, slug }) => (
+  <Hero title={title} subtitle={subtitle} slug={slug} />
 );
 
 export default Home;
