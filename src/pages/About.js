c6dc97cbf37127c983f1b0104e5acf3267188270
tundeiness <tundeiness@yahoo.co.uk>
2020-06-25 23:18:28 +0100

    add axios

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,7 +11,10 @@ const About = ({ title }) => (
     <Content>
       The best way to consume React-Bootstrap is via the npm package which you can install with npm (or yarn if you prefer).
 
-      If you plan on customizing the Bootstrap Sass files, or don't want to use a CDN for the stylesheet, it may be helpful to install vanilla Bootstrap as well.
+      If you plan on customizing the Bootstrap Sass files, or don`
+      t want to use a CDN for the stylesheet, it may be helpful to install vanilla Bootstrap as well.
+
+      In case you are using Sass the simplest way is to include the Bootstrap’s source Sass files in your main Sass file and then require it on your src/index.js or App.js file. This applies to a typical create-react-app application in other use cases you might have to setup the bundler of your choice to compile Sass/SCSS stylesheets to CSS.
     </Content>
   </div>
 );
