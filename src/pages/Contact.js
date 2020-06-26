bb087ac3c489456763c1c691b790d28b67b23998
tundeiness <tundeiness@yahoo.co.uk>
2020-06-26 14:29:16 +0100

    setup form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,13 +1,17 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-useless-constructor */
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Hero from '../Components/Hero';
+import Content from '../Components/content';
 
 class Contact extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       name: '',
-      mesage: '',
+      message: '',
       email: '',
       disabled: false,
       emailSent: null,
@@ -15,7 +19,21 @@ class Contact extends React.Component {
   }
 
   render() {
-    return (<p>Contact me!!</p>);
+    const { title } = this.props;
+    const { name } = this.state;
+    return (
+      <div>
+        <Hero title={title} />
+        <Content>
+          <Form>
+            <Form.Group>
+              <Form.Label htmlFor="full-name"> Full Name</Form.Label>
+              <Form.Control id="full-name" name="name" type="text" value={name} />
+            </Form.Group>
+          </Form>
+        </Content>
+      </div>
+    );
   }
 }
 
