041ef3e3dd8b3190485d638fe0d97cb16bd2fce4
tundeiness <tundeiness@yahoo.co.uk>
2020-06-26 14:16:46 +0100

    setup state for contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,6 +5,13 @@ import React from 'react';
 class Contact extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      name: '',
+      mesage: '',
+      email: '',
+      disabled: false,
+      emailSent: null,
+    };
   }
 
   render() {
