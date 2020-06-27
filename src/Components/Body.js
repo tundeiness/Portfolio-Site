6a38892f068975cf05ca11311268739acb284259
tundeiness <tundeiness@yahoo.co.uk>
2020-06-27 16:50:29 +0100

    adjust responsive value in large screens

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -9,8 +9,8 @@ import Col from 'react-bootstrap/Col';
 const Body = ({ title, subtitle, slug }) => (
   <Container className="p-0 cont-color" fluid>
     <Row className="flex-sm-column flex-lg-row ">
-      <Col className="px-5 row-col-color-1" border="primary" xs={12} lg={2}>1 of 2</Col>
-      <Col className="px-5 row-col-color-2" border="primary" xs={12} lg={10}>2 of 2</Col>
+      <Col className="px-5 row-col-color-1" border="primary" xs={12} lg={5}>1 of 2</Col>
+      <Col className="px-5 row-col-color-2" border="primary" xs={12} lg={7}>2 of 2</Col>
     </Row>
   </Container>
 
