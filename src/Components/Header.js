a16dc7ca593e2cac8b79117e8aefbf124333fef4
tundeiness <tundeiness@yahoo.co.uk>
2020-06-27 20:37:14 +0100

    style avatar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react/prop-types */
 import React from 'react';
 import Row from 'react-bootstrap/Row';
@@ -6,7 +7,9 @@ import Col from 'react-bootstrap/Col';
 const Header = () => (
   <Row className="flex-column justify-content-center row-inner my-5">
     <Col xs={12}>
-      Yaaay!
+      <a href="#" className="d-inline-block image-avatar">
+        <img src="https://user-images.githubusercontent.com/25479050/85928866-43384880-b8a8-11ea-89ab-9d42b1ae286d.jpg" alt="avatar name" />
+      </a>
     </Col>
   </Row>
 );
