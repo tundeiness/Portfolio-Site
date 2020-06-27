7b2fb17ffa90517610192aacb81b5a5493a99cb9
tundeiness <tundeiness@yahoo.co.uk>
2020-06-27 17:01:59 +0100

    WIP header

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -5,12 +5,16 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 // import Hero from './Hero';
 // import Carousel from '../Components/Carousel';
+import Header from './Header';
 
 const Body = ({ title, subtitle, slug }) => (
   <Container className="p-0 cont-color" fluid>
     <Row className="flex-sm-column flex-lg-row ">
-      <Col className="px-5 row-col-color-1" border="primary" xs={12} lg={5}>1 of 2</Col>
-      <Col className="px-5 row-col-color-2" border="primary" xs={12} lg={7}>2 of 2</Col>
+      <Col className="px-5 row-col-color-1" border="primary" xs={12} lg={5}>
+        {/* 1 of 2 Header */}
+        <Header />
+      </Col>
+      <Col className="px-5 row-col-color-2" border="primary" xs={12} lg={7}>2 of 2 Contents</Col>
     </Row>
   </Container>
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-// import Hero from './Hero';
-// import Carousel from '../Components/Carousel';
 
-const Header = ({ title, subtitle, slug }) => (
-  // <Container className="p-0" fluid="true"></Container>
-  <Row lg={2}>
-    {/* <Hero title={title} subtitle={subtitle} slug={slug} /> */}
-    {/* <Carousel /> */}
-    <Col xs={12} sm={12}>lorem</Col>
-  </Row>
+const Header = () => (
+  <p>I am the header</p>
 );
-
 export default Header;
