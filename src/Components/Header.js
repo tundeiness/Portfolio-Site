14c86f27c4c14cd24232decc6a16465bb92b1001
tundeiness <tundeiness@yahoo.co.uk>
2020-06-27 20:43:48 +0100

    center avatar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,7 +7,7 @@ import Col from 'react-bootstrap/Col';
 const Header = () => (
   <Row className="flex-column justify-content-center row-inner my-5">
     <Col xs={12}>
-      <a href="#" className="d-inline-block image-avatar">
+      <a href="#" className="d-inline-block image-avatar mx-2">
         <img src="https://user-images.githubusercontent.com/25479050/85928866-43384880-b8a8-11ea-89ab-9d42b1ae286d.jpg" alt="avatar name" />
       </a>
     </Col>
