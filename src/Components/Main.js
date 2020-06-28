60bdccec29d39cff775e1fa007d9ae7def03eb47
tundeiness <tundeiness@yahoo.co.uk>
2020-06-28 13:58:51 +0100

    add bottom border to section separator

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -6,10 +6,24 @@ import Col from 'react-bootstrap/Col';
 
 const Main = () => (
   <Row className="flex-column">
-    <Col>Section one</Col>
-    <Col>Section two</Col>
-    <Col>Section three</Col>
-    <Col>Section four</Col>
+    <Col className="border-bottom" id="About">
+      <section id="one">
+        <header className="major">
+          <h2>
+            Ipsum lorem dolor aliquam ante commodo
+            <br />
+            magna sed accumsan arcu neque.
+          </h2>
+        </header>
+        <p>Accumsan orci faucibus id eu lorem semper. Eu ac iaculis ac nunc nisi lorem vulputate lorem neque cubilia ac in adipiscing in curae lobortis tortor primis integer massa adipiscing id nisi accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque cubilia.</p>
+        <ul className="actions">
+          <li><a href="#" className="button">Learn More</a></li>
+        </ul>
+      </section>
+    </Col>
+    <Col className="border-bottom">Section two</Col>
+    <Col className="border-bottom">Section three</Col>
+    <Col>Footer</Col>
   </Row>
 );
 export default Main;
