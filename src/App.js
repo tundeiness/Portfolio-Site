cde7f6ae82e83faec5b9da02208204a0e3b292d6
tundeiness <tundeiness@yahoo.co.uk>
2020-06-29 14:11:45 +0100

    disable previous footer structure in App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
+// import Row from 'react-bootstrap/Row';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import './App.css';
 
-import Footer from './Components/Footer';
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import Projects from './pages/Projects';
-import About from './pages/About';
+// import Footer from './Components/Footer';
+// import Home from './pages/Home';
+// import Contact from './pages/Contact';
+// import Projects from './pages/Projects';
+// import About from './pages/About';
 
 // import Header from './Components/Header';
 // import Navigation from './Components/Navigation';
@@ -87,7 +87,7 @@ class App extends React.Component {
 
           <Route path="/contact" exact render={() => <Contact title={contact.title} subtitle={contact.subtitle} slug={contact.slug} />} /> */}
 
-          <Footer />
+          {/* <Footer /> */}
         </Container>
       </Router>
     );
