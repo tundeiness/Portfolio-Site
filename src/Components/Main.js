51c222e1a9b12eaf8ec8349f0fb11a786c8eac42
tundeiness <tundeiness@yahoo.co.uk>
2020-07-01 10:41:29 +0100

    add showModal and hideModal methods -bind the showModal and hiideModal methods

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -13,8 +13,19 @@ class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      show: false,
     };
+
+    this.showModal = this.showModal.bind(this);
+    this.hideModal = this.hideModal.bind(this);
+  }
+
+  showModal() {
+    this.setState({ show: true });
+  }
+
+  hideModal() {
+    this.setState({ show: false });
   }
 
   render() {
