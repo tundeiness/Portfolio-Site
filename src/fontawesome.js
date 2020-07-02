7406ed87392a6dc68ce913b4131d7275931caa02
tundeiness <tundeiness@yahoo.co.uk>
2020-07-02 20:20:23 +0100

    import packages

---
+++ src/fontawesome.js
@@ -0,0 +1,13 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faCode, faHighlighter, faCoffee, fabGithub, fabTwitter,
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+  faMoneyBill,
+  faCode,
+  faHighlighter,
+  faCoffee,
+  fabGithub,
+  fabTwitter,
+);
