2576193865f323790b329eca1d02363ef500b824
tundeiness <tundeiness@yahoo.co.uk>
2020-07-03 16:26:01 +0100

    add styling to set the farther down

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,7 @@ import Col from 'react-bootstrap/Col';
 
 const Header = () => (
   <>
-    <Row className="flex-column  row-inner">
+    <Row className="flex-column  row-inner mb-5 pb-5">
       <Col xs={12} className=" d-flex justify-content-center justify-content-lg-end col-avatar">
         <a href="#" className="d-inline-block image-avatar mx-3">
           <img src="https://user-images.githubusercontent.com/25479050/85928866-43384880-b8a8-11ea-89ab-9d42b1ae286d.jpg" alt="avatar-name" />
