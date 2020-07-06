2664277a02ab5f0f18d55d92a09feef523b56407
tundeiness <tundeiness@yahoo.co.uk>
2020-07-06 17:56:42 +0100

    clean up Main.js

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -12,7 +12,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../fontawesome';
 
 import Contact from '../pages/Contact';
-// import Footer from './Footerx';
 
 class Main extends React.Component {
   constructor(props) {
@@ -132,82 +131,13 @@ class Main extends React.Component {
             <p>Browse through selections of my projects based on the tech stacks I have used.</p>
             <Row className="flex-lg-wrap justify-content-lg-around">
               {Repo}
-              {/* <Col className="project-item" xs={12} lg="6">
-                1 of 4
-                <Card className="project-item-card">
-                  <Card.Img variant="top" src="https://user-images.githubusercontent.com/25479050/85928844-1b48e500-b8a8-11ea-8720-9665d4590d9e.jpg" />
-                  <Button className="d-inline-block project-button" onClick={this.showModal}>View</Button>
-                  <Card.Body>
-                    <Card.Title>Special title treatment</Card.Title>
-                    <Card.Text>
-                      Some quick example text to build on the card title and the card&#39;s content.
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col> */}
-              {/* <Col className="project-item" xs={12} lg="6">
-                2 of 4
-              </Col>
-              <Col className="project-item" xs={12} lg="6">
-                3 of 4
-              </Col>
-              <Col className="project-item" xs={12} lg="6">
-                4 of 4
-              </Col> */}
             </Row>
           </Col>
 
           <Col className="main-col-three" id="next-form">
             <section id="three">
-              {/* <h2>Get In Touch</h2>
-        <p>
-          Accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque lorem ipsum dolor.
-        </p> */}
               <div className="row" id="contact-me">
                 <Contact />
-                {/* <div className="col-8 col-12-small">
-            <form method="post" action="#">
-              <div className="row gtr-uniform gtr-50">
-                <div className="col-6 col-12-xsmall">
-                  <input type="text" name="name" id="name" placeholder="Name" />
-                </div>
-                <div className="col-6 col-12-xsmall">
-                  <input type="email" name="email" id="email" placeholder="Email" />
-                </div>
-                <div className="col-12">
-                  <textarea name="message" id="message" placeholder="Message" rows="4" />
-                </div>
-              </div>
-            </form>
-            <ul className="actions">
-              <li><input type="submit" value="Send Message" /></li>
-            </ul>
-          </div> */}
-
-                {/* <div className="col-4 col-12-small">
-            <ul className="labeled-icons">
-              <li>
-                <h3 className="icon solid fa-home"><span className="label">Address</span></h3>
-                1234 Somewhere Rd.
-                <br />
-                Nashville, TN 00000
-                <br />
-                United States
-              </li>
-              <li>
-                <h3 className="icon solid fa-mobile-alt">
-                  <span className="label">Phone</span>
-                </h3>
-                000-000-0000
-              </li>
-              <li>
-                <h3 className="icon solid fa-envelope">
-                  <span className="label">Email</span>
-                </h3>
-                <a href="#">hello@untitled.tld</a>
-              </li>
-            </ul>
-          </div> */}
               </div>
             </section>
           </Col>
