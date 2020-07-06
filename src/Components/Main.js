d27db4da8cea30defe4edd82261101eb65f6c434
tundeiness <tundeiness@yahoo.co.uk>
2020-07-06 17:57:49 +0100

    clean up Main.js

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,6 +1,4 @@
 /* eslint-disable max-len */
-/* eslint-disable jsx-a11y/anchor-is-valid */
-/* eslint-disable react/prop-types */
 import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
