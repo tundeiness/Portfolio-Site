12c7c66c85425b2cfe5cf04a7946520d989070ef
tundeiness <tundeiness@yahoo.co.uk>
2020-07-07 10:35:27 +0100

    add fixed image height and width for Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,7 +8,12 @@ const Header = () => (
     <Row className="flex-column  row-inner mb-5 pb-5">
       <Col xs={12} className=" d-flex justify-content-center justify-content-lg-end col-avatar">
         <a href="#" className="d-inline-block image-avatar mx-3">
-          <img src="https://user-images.githubusercontent.com/25479050/85928866-43384880-b8a8-11ea-89ab-9d42b1ae286d.jpg" alt="avatar-name" />
+          <img
+            src="https://user-images.githubusercontent.com/25479050/85928866-43384880-b8a8-11ea-89ab-9d42b1ae286d.jpg"
+            width={123}
+            height={123}
+            alt="avatar-name"
+          />
         </a>
       </Col>
       <Col className="d-flex justify-content-center justify-content-sm-center justify-content-md-center justify-content-lg-end header-col" xs={12}>
