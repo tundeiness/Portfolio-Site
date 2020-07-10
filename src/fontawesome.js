f1a617dbe276de656b4b17acfe736f9916060d93
tundeiness <tundeiness@yahoo.co.uk>
2020-07-10 15:49:49 +0100

    remove excess code line fontawesome.js

--- src/fontawesome.js
+++ src/fontawesome.js
@@ -7,9 +7,8 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import {
   faGithubAlt, faTwitter, faAngellist, faLinkedinIn, fab, faCodepen, faBootstrap, faGit, faReact, faHtml5, faCss3,
 } from '@fortawesome/free-brands-svg-icons';
-import { faHeartbeat, faCircle } from '@fortawesome/free-solid-svg-icons';
+import { faHeartbeat } from '@fortawesome/free-solid-svg-icons';
 
-// fab represents the entire brand icon package.
 library.add(
   fab,
   faGithubAlt,
