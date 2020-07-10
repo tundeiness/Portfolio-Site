1083455f2085e36e6b8192241f01eea72a1013f1
tundeiness <tundeiness@yahoo.co.uk>
2020-07-10 15:47:24 +0100

    refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-/* eslint-disable max-len */
-/* eslint-disable react/jsx-filename-extension */
-/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import './App.css';
 import Body from './Components/Body';
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <Container className="p-0" fluid="true">
-          <Body />
-        </Container>
-      </>
-    );
-  }
-}
+const App = () => (
+  <>
+    <Container className="p-0" fluid="true">
+      <Body />
+    </Container>
+  </>
+);
 
 export default App;
