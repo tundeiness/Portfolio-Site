4bae52957997d1dd60b1e539897e2485f9698508
tundeiness <tundeiness@yahoo.co.uk>
2020-10-06 14:29:51 +0100

    add react google analytics tracking

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+const trackingId = 'UA-171811390-1';
+ReactGA.initialize(trackingId);
 
 ReactDOM.render(
   <React.StrictMode>
